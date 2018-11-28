dd11a955a286ebcef79e9296ab9985f4673601f6
Roman896 <romanturner8966@gmail.com>
2018-11-28 15:39:04 -0600

    fixed app

---
+++ squareRoot.js
@@ -0,0 +1,9 @@
+function findsquareRoot() {
+
+var theNumber=(prompt("enter in a number"));
+
+var sqrrt = theNumber*theNumber;
+document.getElementById("demo").innerHTML = sqrrt;
+
+console.log(6+theNumber*theNumber);
+}
